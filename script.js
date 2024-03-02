c93f0a36247ccba1b5f2872858541649bbdfaa39
5huv0 <md88748874@gmail.com>
2024-03-03 00:35:43 +0600

    adding js

--- script.js
+++ script.js
@@ -1,6 +1,96 @@
-const cardTitle = document.getElementsByClassName("card-title").innerHTML;
-
-function cardtitle(){
-    console.log(cardTitle);
+function player1(){
+    let s = document.getElementById("p1").innerText;
+    console.log(s);
 }
-
+function player2(){
+    let s = document.getElementById("p2").innerText;
+    console.log(s);
+}
+function player3(){
+    let s = document.getElementById("p3").innerText;
+    console.log(s);
+}
+function player4(){
+    let s = document.getElementById("p4").innerText;
+    console.log(s);
+}
+function player5(){
+    let s = document.getElementById("p5").innerText;
+    console.log(s);
+}
+function player6(){
+    let s = document.getElementById("p6").innerText;
+    console.log(s);
+}
+function player7(){
+    let s = document.getElementById("p7").innerText;
+    console.log(s);
+}
+function player8(){
+    let s = document.getElementById("p8").innerText;
+    console.log(s);
+}
+function player9(){
+    let s = document.getElementById("p9").innerText;
+    console.log(s);
+}
+function player10(){
+    let s = document.getElementById("p10").innerText;
+    console.log(s);
+}
+function player11(){
+    let s = document.getElementById("p11").innerText;
+    console.log(s);
+}
+function player12(){
+    let s = document.getElementById("p12").innerText;
+    console.log(s);
+}
+function player13(){
+    let s = document.getElementById("p13").innerText;
+    console.log(s);
+}
+function player14(){
+    let s = document.getElementById("p14").innerText;
+    console.log(s);
+}
+function player15(){
+    let s = document.getElementById("p15").innerText;
+    console.log(s);
+}
+function player16(){
+    let s = document.getElementById("p16").innerText;
+    console.log(s);
+}
+function player17(){
+    let s = document.getElementById("p17").innerText;
+    console.log(s);
+}
+function player18(){
+    let s = document.getElementById("p18").innerText;
+    console.log(s);
+}
+function player19(){
+    let s = document.getElementById("p19").innerText;
+    console.log(s);
+}
+function player20(){
+    let s = document.getElementById("p20").innerText;
+    console.log(s);
+}
+function player21(){
+    let s = document.getElementById("p21").innerText;
+    console.log(s);
+}
+function player22(){
+    let s = document.getElementById("p22").innerText;
+    console.log(s);
+}
+function player23(){
+    let s = document.getElementById("p23").innerText;
+    console.log(s);
+}
+function player24(){
+    let s = document.getElementById("p24").innerText;
+    console.log(s);
+}
